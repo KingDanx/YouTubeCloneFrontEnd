84fc45eda9e9b171d07382ce5639d6e1b787e32d
Dan Prudhomme <dannyprud@yahoo.com>
2021-11-18 18:34:49 -0600

    Import axios, set up react for use with hooks, delete unused files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+
 function App() {
+  
+  const [comments, setComments] = useState([]);
+  
+  const [counter, setCounter] = useState(0);
+
+
+
+  const getAllComments = async()=>{
+    await axios.get("http://localhost:5000/api/comments/")
+    .then((res) => {
+      setComments(res.data);
+      console.log(res.data);
+    });
+  }
+
+  useEffect(()=> getAllComments(),[counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{counter}</p>
+      <button onClick={()=> setCounter(counter+1)}>hi</button>
     </div>
   );
 }
