333f6b56cbdaa12145db5faca95f64a600ecd15c
Dan Prudhomme <dannyprud@yahoo.com>
2021-11-26 12:39:05 -0600

    thumb up and thumb down button integration

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,25 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbDownOffAltIcon from '@mui/icons-material/ThumbDownOffAlt';
 
-function Comments({comments, videoId, addLike, addDislike}){
-        
-    useEffect(() => {
-        console.log(comments)
-    }, [comments]);
-    
-    return(
-       <div>
-           {comments.slice(0).reverse().map((comment, index) => videoId === comment.videoID ? <div className="commnets-map-div" key={index}>
-               <span>{comment.text} </span>
-               <div className="comments-button-right">
-                    <span className="comments-dislikes-span">{comment.dislikes}</span><button onClick={()=>addDislike(comment)}>Dislike</button>
-               </div>
-               <div className="comments-button-right">
-                    <span className="comments-likes-span">{comment.likes}</span><button className="comments-likes-button" onClick={()=>addLike(comment)}>Like</button>    
-               </div> 
-               </div>  : null)}
-       </div>
-        
-    );
+function Comments({ comments, videoId, addLike, addDislike }) {
+  useEffect(() => {
+    console.log(comments);
+  }, [comments]);
+
+  return (
+    <div>
+      {comments
+        .slice(0)
+        .reverse()
+        .map((comment, index) =>
+          videoId === comment.videoID ? (
+            <div className="commnets-map-div" key={index}>
+              <span>{comment.text} </span>
+              <div className="comments-button-right">
+                <span className="comments-dislikes-span">
+                  {comment.dislikes}
+                </span>
+                <ThumbDownOffAltIcon 
+                className="comments-likes-button"
+                onClick={() => addDislike(comment)} 
+                />
+              </div>
+              <div className="comments-button-right">
+                <span className="comments-likes-span">{comment.likes}</span>
+                <ThumbUpOffAltIcon
+                  className="comments-likes-button"
+                  onClick={() => addLike(comment)}
+                />
+                </div>
+            </div>
+          ) : null
+        )}
+    </div>
+  );
 }
 
 export default Comments;
